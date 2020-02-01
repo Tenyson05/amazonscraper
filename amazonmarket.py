68eb64c3c6887afa52d9a73c56afa390f94d4887
tenyson05 <rojahog@hotmail.com>
2020-02-01 11:33:25 -0500

    Search functionality added

--- amazonmarket.py
+++ amazonmarket.py
@@ -4,6 +4,10 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 URL = "https://www.amazon.com/"
+search_word = str(raw_input("Enter your search word:\n"))
+
 
 driver = webdriver.Firefox()
 driver.get(URL)
+query = driver.find_element_by_xpath('//*[@id="twotabsearchtextbox"]')
+query.send_keys(search_word, Keys.ENTER)
